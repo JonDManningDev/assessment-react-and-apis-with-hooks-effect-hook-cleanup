78833fdee95021ecc48587362375cec3ad2f5d55
Jonathan <jondmanning1986@gmail.com>
2024-10-23 15:11:42 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+
+  // Load data from https://jsonplaceholder.typicode.com/todos?userId=3
+
+  useEffect(() => {
+
+    const abortController = new AbortController;
+
+    async function loadUser () {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos?userId=3",
+          { signal: abortController.signal }
+        );
+        const data = await response.json();
+        setToDos(data);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Fetch request aborted.");
+        } else {
+          console.error("Fetch error:", error);
+        }
+      }
+    }
+
+    loadUser();
+
+    return () => abortController.abort();
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <ul className="todo-list">
+        {toDos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "",
+            }}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
